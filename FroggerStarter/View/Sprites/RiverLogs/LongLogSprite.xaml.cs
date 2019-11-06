b701b92abb3d1f227fdec9f273112f1b8fa0bcc2
Michael Pavich <mpavich2@my.westga.edu>
2019-11-06 15:00:21 -0500

    Added log sprites for river crossing

---
+++ FroggerStarter/View/Sprites/RiverLogs/LongLogSprite.xaml.cs
@@ -0,0 +1,23 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FroggerStarter.View.Sprites.RiverLogs
+{
+    /// <summary>
+    ///     Stores information for the LongLogSprite class
+    /// </summary>
+    /// <seealso cref="FroggerStarter.View.Sprites.BaseSprite" />
+    public sealed partial class LongLogSprite
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LongLogSprite" /> class.
+        /// </summary>
+        public LongLogSprite()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ FroggerStarter/View/Sprites/RiverLogs/ShortLogSprite.xaml.cs
@@ -0,0 +1,23 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FroggerStarter.View.Sprites.RiverLogs
+{
+    /// <summary>
+    ///     Stores information for the ShortLogSprite class
+    /// </summary>
+    /// <seealso cref="FroggerStarter.View.Sprites.BaseSprite" />
+    public sealed partial class ShortLogSprite
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ShortLogSprite" /> class.
+        /// </summary>
+        public ShortLogSprite()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
