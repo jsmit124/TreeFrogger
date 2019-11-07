18fc73f28f0fd68dacb4330f6082ff1b15a0c062
jsmit124 <jsmit124@my.westga.edu>
2019-11-07 15:07:36 -0500

    Implemented RelayCommand class

---
+++ FroggerStarter/Utility/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FroggerStarter.Utility
+{
+    /// <summary>Stores information for the Relay Command class</summary>
+    /// <seealso cref="System.Windows.Input.ICommand" />
+    public class RelayCommand : System.Windows.Input.ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        /// <summary>Defines the method that determines whether the command can execute in its current state.</summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            bool result = canExecute?.Invoke(parameter) ?? true;
+            return result;
+        }
+
+        /// <summary>Defines the method to be called when the command is invoked.</summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                execute(parameter);
+            }
+        }
+
+        /// <summary>Occurs when changes occur that affect whether or not the command should execute.</summary>
+        /// <returns>True if can execute change; false otherwise</returns>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>Initializes a new instance of the <see cref="RelayCommand"/> class.</summary>
+        /// <param name="execute">The execute.</param>
+        /// <param name="canExecute">The can execute.</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
+        /// <summary>Called when [can execute changed].</summary>
+        /// Typically, protected but made public, so can trigger a manual refresh on the result of CanExecute.
+        public virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
