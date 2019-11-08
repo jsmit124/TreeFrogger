a8d9fb0ab48ef5474300033ced8f3f8ca4365831
Michael Pavich <mpavich2@my.westga.edu>
2019-11-08 14:22:21 -0500

    Refactored power up class

--- FroggerStarter/Model/PowerUps/PowerUp.cs
+++ FroggerStarter/Model/PowerUps/PowerUp.cs
@@ -6,14 +6,12 @@ namespace FroggerStarter.Model.PowerUps
     /// <seealso cref="FroggerStarter.Model.BaseObject" />
     public abstract class PowerUp : BaseObject
     {
+        #region Properties
+
         /// <summary>Gets or sets the type of the power up.</summary>
         /// <value>The type of the power up.</value>
         public PowerUpType PowerUpType { get; protected set; }
 
-        /// <summary>Initializes a new instance of the <see cref="PowerUp"/> class.</summary>
-        /// <param name="type">The type.</param>
-        protected PowerUp()
-        {
-        }
+        #endregion
     }
 }
