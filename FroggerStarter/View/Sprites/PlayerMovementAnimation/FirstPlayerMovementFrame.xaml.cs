3b6aecfaa8205ecb20a897476a88abaf7f0d2abb
jsmit124 <jsmit124@my.westga.edu>
2019-11-08 14:49:20 -0500

    Added player movement animation sprite

---
+++ FroggerStarter/View/Sprites/PlayerMovementAnimation/FirstPlayerMovementFrame.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FroggerStarter.View.Sprites.PlayerMovementAnimation
+{
+    /// <summary>
+    /// Class storing information for the first player movement frame
+    /// </summary>
+    public sealed partial class FirstPlayerMovementFrame
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstPlayerMovementFrame"/> class.
+        /// </summary>
+        public FirstPlayerMovementFrame()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
